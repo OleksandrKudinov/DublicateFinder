be4649b09e61cc37c0149d4272183d11260e2f2c
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-02-05 17:28:48 +0200

    Updated main logic;

--- DublicateFinder/DublicateFinder.Cmd/Program.cs
+++ DublicateFinder/DublicateFinder.Cmd/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace DublicateFinder.Cmd
 {
@@ -11,21 +12,26 @@ namespace DublicateFinder.Cmd
         {
             Debug.WriteLine(message);
         }
-    } 
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
             String root = @"D:\";
+            Int32 minSize = 1;
+            Int32 maxSize = 1000;
+
             FileFinder provider = new FileFinder(new DebugOutput());
 
-            IEnumerable<FileInfo> files = provider.GetAllFiles(root);
+            FileInfo[] files = provider.GetAllFiles(root)
+                .Where(file => file.Length >= minSize && file.Length <= maxSize)
+                .Take(100)
+                .ToArray();
 
-            Console.WriteLine("Files:");
-            foreach (FileInfo file in files)
+            foreach(FileInfo file in files)
             {
-                Console.WriteLine(file.Length);
+                Console.WriteLine(file.FullName);
             }
         }
     }
