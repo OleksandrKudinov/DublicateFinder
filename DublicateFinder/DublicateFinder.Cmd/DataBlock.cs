5899ad92f86bf9926eeb8027c79a7612945ed8fd
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-02-05 20:16:30 +0200

    Added class DataBlock. DataBlock is like part of file, which is comparable.

---
+++ DublicateFinder/DublicateFinder.Cmd/DataBlock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DublicateFinder.Cmd
+{
+    internal sealed class DataBlock : IComparable<DataBlock>, IComparable
+    {
+        public static Int32 MaxSize = 4096;
+
+        public DataBlock(Byte[] block)
+        {
+            _buffer = block;
+        }
+
+        public Int32 CompareTo(DataBlock other)
+        {
+            Byte[] otherBuffer = other._buffer;
+            if (otherBuffer.Length != _buffer.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (Int32 i = 0; i < _buffer.Length; i++)
+            {
+                if (_buffer[i] != otherBuffer[i])
+                {
+                    return _buffer[i] > otherBuffer[i] ? 1 : -1;
+                }
+            }
+
+            return 0;
+        }
+
+        public Int32 CompareTo(object obj)
+        {
+            return CompareTo(obj as DataBlock);
+        }
+
+        private readonly byte[] _buffer;
+    }
+}
