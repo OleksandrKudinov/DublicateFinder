a624108a9e2d8dc8c35f9ed484c98a747517647b
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-02-05 20:24:53 +0200

    Test code;

--- DublicateFinder/DublicateFinder.Cmd/Program.cs
+++ DublicateFinder/DublicateFinder.Cmd/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DublicateFinder.Cmd
 {
@@ -16,23 +18,74 @@ namespace DublicateFinder.Cmd
 
     class Program
     {
+        static volatile Int32 count = 0;
+        static volatile Int32 total = 0;
+
         static void Main(string[] args)
         {
-            String root = @"D:\";
+            String root = @"D:\test1";
             Int32 minSize = 1;
-            Int32 maxSize = 1000;
+            Int32 maxSize = Int32.MaxValue;
 
             FileFinder provider = new FileFinder(new DebugOutput());
 
+            Func<FileInfo, Boolean> filter = file =>
+            {
+                Boolean isOk = file.Length >= minSize && file.Length <= maxSize;
+                total++;
+                if (isOk)
+                {
+                    count++;
+                }
+                return isOk;
+            };
+
+            // like progress bar
+            CancellationTokenSource source = new CancellationTokenSource();
+            Task.Run(async () =>
+            {
+                var token = source.Token;
+
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine($"{nameof(total)} : {total}, {nameof(count)} : {count}");
+                    await Task.Delay(100);
+                }
+                Console.WriteLine($"{nameof(total)} : {total}, {nameof(count)} : {count}");
+                Console.WriteLine("canceled or stopped");
+            });
+
             FileInfo[] files = provider.GetAllFiles(root)
-                .Where(file => file.Length >= minSize && file.Length <= maxSize)
-                .Take(100)
+                .Where(filter)
+                .Take(1000)
                 .ToArray();
 
-            foreach(FileInfo file in files)
+            source.Cancel();
+
+            IEnumerable<FileWrapper> wrapFiles = files.Select(fileinfo => new FileWrapper(fileinfo));
+
+            IEnumerable<IEnumerator<DataBlock>> blockEnumerators = wrapFiles
+                .Select(fileWrapper => fileWrapper.GetEnumerator())
+                .ToArray();
+
+            // Read first DataBlock for each file 
+            if (!blockEnumerators.All(x => x.MoveNext()))
             {
-                Console.WriteLine(file.FullName);
+                return;
             }
+
+            foreach (var block in blockEnumerators.OrderBy(x => x.Current))
+            {
+                Console.WriteLine(block.Current.Buffer.Length);
+            }
+
+            // group by size
+            foreach (var group in files.GroupBy(x => x.Length).Where(x => x.Count() > 1))
+            {
+                Console.WriteLine($"{group.Key} {group.Count()}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
